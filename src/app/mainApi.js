a693ae1c831a7ff7d0678a5e48454f3fff1159cc
loyarabhishek92 <loyarabhishek92@gmail.com>
2026-09-20 17:57:05 +0545

    deploy error fixing

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -2,12 +2,13 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 import { createApi } from "@reduxjs/toolkit/query/react";
 
 
-export const base = 'http://192.168.16.101:5000';
+// export const base = 'http://192.168.16.101:5000';
+export const base = 'https://hospital-nlk9.onrender.com';
 
 export const mainApi = createApi({
     reducerPath: 'mainApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://192.168.16.101:5000/api'
+        baseUrl: 'https://hospital-nlk9.onrender.com/api'
     }),
     endpoints: (builder) => ({}),
 });
